f71512fcb655dc102ac033ccd39ad2fe26986aa3
jacekpietrzak <jacekpietrzak@me.com>
2022-10-31 22:14:54 +0100

    01-Gallery finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+
+gallery.addEventListener('click', selectItem);
+
+function selectItem(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+console.log('anything');
+
+// player.on('timeupdate', function (time) {
+//   console.log('time', time);
+// });
