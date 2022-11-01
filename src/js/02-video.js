eedfc4949d2994d72533c8d605f1190af5692cd6
jacekpietrzak <jacekpietrzak@me.com>
2022-11-01 21:04:25 +0100

    HW 07 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,38 @@
+const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-console.log('anything');
-
-// player.on('timeupdate', function (time) {
-//   console.log('time', time);
+// player.on('play', function () {
+//   console.log('played video!');
 // });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    // console.log('time', time.seconds);
+    localStorage.setItem('videoplayer-current-time', time.seconds);
+  }, 1000)
+);
+
+console.log(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+const throttle = require('lodash.throttle');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const submitBtn = document.querySelector('button');
 
+let feedbackForm = {
+  email: '',
+  message: '',
+};
+
+email.addEventListener('input', event => {
+  const email = event.target.value;
+  feedbackForm = { ...feedbackForm, email: email };
+  //   console.log('email:', event.target.value);
+  throttledSaveStateToLocalStorage();
+});
+
+message.addEventListener('input', event => {
+  const message = event.target.value;
+  feedbackForm = { ...feedbackForm, message: message };
+  throttledSaveStateToLocalStorage();
+});
+
+function saveStateToLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
+}
+
+const throttledSaveStateToLocalStorage = throttle(saveStateToLocalStorage, 500);
+
+const savedSettings = localStorage.getItem('feedback-form-state');
+const parsedSettings = JSON.parse(savedSettings);
+
+email.value = parsedSettings.email;
+message.value = parsedSettings.message;
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+  console.log(parsedSettings);
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  message.value = '';
+});
