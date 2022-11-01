f371491d645d80b3bc7af5a0ca6ad2bf8738ce7a
jacekpietrzak <jacekpietrzak@me.com>
2022-11-01 20:04:48 +0000

    Deploying to gh-pages from @ jacekpietrzak/goit-js-hw-08@eedfc4949d2994d72533c8d605f1190af5692cd6 🚀

--- 02-video.9af0185c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),console.log("anything");
-//# sourceMappingURL=02-video.9af0185c.js.map
--- 02-video.bf40050a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),console.log("anything")}();
-//# sourceMappingURL=02-video.bf40050a.js.map
